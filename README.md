5175041a4830d6b57470128cab0b888d4964d48c
Eduardo Patrocinio <epatro@gmail.com>
2016-06-11 11:44:26 -0700

    First version, but it's awesome!

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# test-mongodb
+Bluemix-MongoDB-Node.js
+===
+This code creates a Bluemix Node.js application with MongoDB as the backend. This app allows you to perform simple CRUD operations on MongoDB using REST APIs
---
+++ test-mongodb.sh
@@ -0,0 +1,42 @@
+#/bin/bash
+# Inspired by https://hub.jazz.net/project/communitysample/postgresql-nodejs/overview
+
+APP_NAME=patrocinio-test-mongodb
+
+# Parameters
+# $1: app name
+# $2: name
+check_url () {
+# Grab URL
+URL=`cf app $APP_NAME | grep urls | awk '{print $2}'`
+
+# Check response
+LINE=`wget --server-response --content-on-error=off ${URL} 2>&1 | grep HTTP`
+
+# Prints result
+if [[ "$LINE" == *"200"* ]]; then
+echo $2 working great!
+else
+echo $2 failed :-/
+fi
+}
+
+# Push the application
+cf push $APP_NAME --no-start --random-route
+
+# Bind the app to the service
+SERVICE_NAME=mongodb
+cf bs $APP_NAME $SERVICE_NAME
+
+if [[ $? -ne 0 ]];
+then
+    echo Failed to bind $SERVICE_NAME to $APP_NAME
+    echo Ensure we have a service called $SERVICE_NAME
+else
+  # Start the app
+  echo Starting the application...
+  cf start $APP_NAME
+
+  # Check response
+  check_url $APP_NAME "MongoDB"
+fi
